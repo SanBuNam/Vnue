3e7fc8efb0f3aaf93337c376d69173788333bedb
sanbunam <softdavidlee@gmail.com>
2021-02-07 08:38:39 -0800

    Nav.js

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fb49d292576c4b923578";
+/******/ 	var hotCurrentHash = "3fbeec605a681d9ff859";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,7 +1,7 @@
 export default function ProductsPage() {
   return (
     <div>
-      <p>Hello!</p>
+      <p>prodcut!</p>
     </div>
   );
 }
